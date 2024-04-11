b88519951f0db81c4f605d7ba7734895d51a3ae1
nhn-academy-marco <marco@ nhnacademy.com>
2024-04-11 15:05:28 +0900

    오타수정

--- src/main/java/com/nhnacademy/http/request/HttpRequest.java
+++ src/main/java/com/nhnacademy/http/request/HttpRequest.java
@@ -10,18 +10,18 @@ public interface HttpRequest {
     //?page=1&sort=age, ex) getParameter("sort") , return age
     String getParameter(String name);
 
-    // paramter를 map 형태로 반환 합니다.
+    // paramter를 map 형태로 반환합니다.
     Map<String, String[]> getParameterMap();
 
-    //header의 value를 반환 합니다.
+    //header의 value를 반환합니다.
     String getHeader(String name);
 
-    //request에 값을(name->value) 설정 합니다., view(html)에 값을 전달 하기 위해서 사용 합니다.
+    //request에 값을(name->value) 설정합니다., view(html)에 값을 전달 하기 위해서 사용 합니다.
     void setAttribute(String name, Object o);
 
     //request에 설정된 값을 반환합니다.
     Object getAttribute(String name);
 
-    //요청 경로를 반환 합니다. GET /index.html?page=1 -> /index.html
+    //요청 경로를 반환합니다. GET /index.html?page=1 -> /index.html
     String getRequestURI();
 }
