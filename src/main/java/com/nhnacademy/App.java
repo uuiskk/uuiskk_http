a189fef6ffebb85f90f5064dda9b1a2716ac2438
nhn-academy-marco <marco@ nhnacademy.com>
2024-04-12 17:47:51 +0900

    copyright 추가

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -1,11 +1,8 @@
 package com.nhnacademy;
 
-import com.nhnacademy.http.HttpRequestHandler;
 import com.nhnacademy.http.SimpleHttpServer;
 import lombok.extern.slf4j.Slf4j;
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 @Slf4j
 public class App 
